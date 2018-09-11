e59d5f3722c047ee477848679171e0815774809b
Forest Guhareza <arza.3d@gmail.com>
2018-09-11 12:25:23 +0700

    try fix onclick event to on for mobile

--- ar3.js
+++ ar3.js
@@ -461,7 +461,8 @@
                 //#######
                 // <h3> # ATTACH EVENT (accordion)
                 //#######
-                $h3.click(function(){
+                //$h3.click(function(){
+                $h3.on('click touchstart', function(){            
                     if (this.classList.contains("r3-active") !== true) {
                         this.style.setProperty('-webkit-transition','background-color 0.3s ease-in-out 0.6s, border-radius 0.2s 0.6s, color 0.3s ease-in-out 0.6s');
                         this.style.setProperty('transition','background-color 0.3s ease-in-out 0.6s, border-radius 0.2s 0.6s, color 0.3s ease-in-out 0.6s');
