9c545ba852470a3c7c46e629ffeb51fd8816d0b4
Forest Guhareza <arza.3d@gmail.com>
2018-09-11 15:59:45 +0700

    change touchstart to touch

--- ar3.js
+++ ar3.js
@@ -461,8 +461,7 @@
                 //#######
                 // <h3> # ATTACH EVENT (accordion)
                 //#######
-                //$h3.click(function(){
-                $h3.on('click touchstart', function(){            
+                $h3.on('click touch', function(){
                     if (this.classList.contains("r3-active") !== true) {
                         this.style.setProperty('-webkit-transition','background-color 0.3s ease-in-out 0.6s, border-radius 0.2s 0.6s, color 0.3s ease-in-out 0.6s');
                         this.style.setProperty('transition','background-color 0.3s ease-in-out 0.6s, border-radius 0.2s 0.6s, color 0.3s ease-in-out 0.6s');
