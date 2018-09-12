a75cca1b20b20513a01941c6ddb05981e8d823e4
Forest Guhareza <arza.3d@gmail.com>
2018-09-12 09:49:54 +0700

    add web logo

--- ar3header.js
+++ ar3header.js
@@ -1,3 +1,10 @@
 (function() {
-  $('body').prepend('<nav>lol</nav>');
+  $('body').prepend('<nav style="z-index:10;position:absolute;top:0px;left:0px;background-color:rgb(23,26,28);width:100%;height:45px;">\n'+
+    '<div style="width:158px;height:100%;background-color:rgba(0,0,0,0);float:left;">\n'+
+      '<img src="https://image.ibb.co/i5Hp0a/LogoWeb.png" alt="">\n'+
+    '</div>\n'+
+    '<div style="float:left;width:2px;height:100%;background-color:rgba(0,0,0,0.4);"></div>\n'+
+    '<div style="float:right;width:2px;height:100%;background-color:rgba(0,0,0,0.4);"></div>\n'+
+  '</nav>\n'+
+  '<div style="clear:left"></div><br><br>');
 })();
