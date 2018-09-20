b6fab09dead14191ee8d83143ebdfa5710d7383d
arza.3d <arza.3d@gmail.com>
2018-09-20 22:19:04 +0700

    update ar3-short-code

--- modular/short-code/ar3-short-code.js
+++ modular/short-code/ar3-short-code.js
@@ -1,23 +1,29 @@
 // ar3-short-code.js
 // required jQuery
 // create <button>
-if (!$('body').hasClass('r3-short-code_done')) {
-    $('pre + pre').css('display', 'none')
-        .before('<button class="r3-short-code">'+
-        'show all code</button>');
-}
-$('body').addClass('r3-short-code_done');
+(function() {
+    'use strict';
+    var shortCode = 'short code',
+        showAllCode = 'show all code',
+        newElement = !$('body').hasClass('r3-short-code_done');
 
-// attach event
-$('.r3-short-code').click(function() {
-    if ($(this).hasClass('r3-active')) {
-        this.innerHTML= 'show all code';
-        $(this).prev().delay(300).toggle(600);
-        $(this).next().toggle(300);
-    } else {
-        this.innerHTML= 'short code';
-        $(this).prev().toggle(300);
-        $(this).next().delay(300).toggle(600);
+    if (newElement) {
+        $('pre + pre').css('display', 'none')
+            .before('<button class="r3-short-code">'+ shortCode + '</button>');
     }
-    $(this).toggleClass('r3-active');
-});
+
+    $('.r3-short-code').click(function() {
+        if ($(this).hasClass('r3-active')) {
+            this.innerHTML= showAllCode;
+            $(this).prev().delay(300).toggle(600);
+            $(this).next().toggle(300);
+        } else {
+            this.innerHTML= shortCode;
+            $(this).prev().toggle(300);
+            $(this).next().delay(300).toggle(600);
+        }
+        $(this).toggleClass('r3-active');
+    });
+
+    $('body').addClass('r3-short-code_done');
+})();
--- modular/tab/ar3-tab.js
+++ modular/tab/ar3-tab.js
@@ -1,76 +1,69 @@
-//##############################################
-// variable outside .tab-r3 functionality      #
-// to make sure element will not be construct  #
-// if it is already construct                  #
-//############################################## initialized
-var $ar3 = $('body'),
-    newElement = !$ar3.hasClass('no_more_new_element');
+(function( {
 
-//##########
-// .tab-r3 # contain 1st div that will be the links, and 2nd div that will be the content
-//##########
-(function() {
+    var newElement = !$('body').hasClass('r3-tab_done');
 
-    var $tabGroup = $('.tab-r3'),
-        $div2 = $tabGroup.children('div:nth-child(2)'),
-        $divContent = $div2.children('div'),
-        divContentL = $divContent.length;
-
-    //###########
-    // <div>2nd # access div content
-    //###########
+    //##########
+    // .tab-r3 # contain 1st div that will be the links, and 2nd div that will be the content
+    //##########
     (function() {
-        for (i = 0; i < divContentL; i++) {
-            $($divContent[i]).attr('id', 'r3_tab_content_'+ i);
-        }
-    })();
 
-    //###########
-    // <div>1st #
-    //###########
-    (function() {
-        var $div1 = $tabGroup.children('div:first-child'),
-            $a = $div1.children('a');
+        var $tabGroup = $('.tab-r3'),
+            $div2 = $tabGroup.children('div:nth-child(2)'),
+            $divContent = $div2.children('div'),
+            divContentL = $divContent.length;
 
-        //######
-        // <a> # create href attribute to <a>
-        //######
+        //###########
+        // <div>2nd # access div content
+        //###########
         (function() {
-            if (newElement) {
-                for (i = 0; i < divContentL; i++) {
-                    var id = $($divContent[i]).attr('id');
-                    $a[i].setAttribute('href', '#'+id);
+            for (i = 0; i < divContentL; i++) {
+                $($divContent[i]).attr('id', 'r3_tab_content_'+ i);
+            }
+        })();
+
+        //###########
+        // <div>1st #
+        //###########
+        (function() {
+            var $div1 = $tabGroup.children('div:first-child'),
+                $a = $div1.children('a');
+
+            //######
+            // <a> # create href attribute to <a>
+            //######
+            (function() {
+                if (newElement) {
+                    for (i = 0; i < divContentL; i++) {
+                        var id = $($divContent[i]).attr('id');
+                        $a[i].setAttribute('href', '#'+id);
+                    }
                 }
-            }
-        })();
+            })();
 
-        //##################
-        // <a>:first-child # first <a> is activated by default
-        //##################
-        (function() {
-            if (newElement) {
-                $div1.children('a:first-child').addClass('r3-active');
-            }
-        })();
+            //##################
+            // <a>:first-child # first <a> is activated by default
+            //##################
+            (function() {
+                if (newElement) {
+                    $div1.children('a:first-child').addClass('r3-active');
+                }
+            })();
 
-        //######
-        // <a> # attach click event
-        //######
-        $a.click(function() {
-            var id = this.href.slice(this.href.indexOf('#') + 1, this.href.length);
-            $(this).siblings().removeClass('r3-active');
-            $(this).addClass('r3-active');
-            $('#'+id).siblings().fadeOut(400);
-            $('#'+id).delay(401).fadeIn();
-        });
+            //######
+            // <a> # attach click event
+            //######
+            $a.click(function() {
+                var id = this.href.slice(this.href.indexOf('#') + 1, this.href.length);
+                $(this).siblings().removeClass('r3-active');
+                $(this).addClass('r3-active');
+                $('#'+id).siblings().fadeOut(400);
+                $('#'+id).delay(401).fadeIn();
+            });
+
+        })();
 
     })();
 
-})();
+    $('body').addClass('r3-tab_done');
 
-//##############################################
-// variable outside .tab-r3 functionality      #
-// to make sure element will not be construct  #
-// if it is already construct                  #
-//############################################## check
-$ar3.addClass('no_more_new_element'); 
+}))();
