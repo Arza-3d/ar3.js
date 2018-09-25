a3ac00f89b1cdf6135b57304f6df1c5a23495884
arza.3d <arza.3d@gmail.com>
2018-09-25 08:57:52 +0700

    perbaiki struktur ,untangled, header ga harus dalam .ar3

--- ar3header.js
+++ ar3header.js
@@ -3,7 +3,9 @@
     '<div style="width:158px;height:100%;background-color:rgba(0,0,0,0);float:left;">\n'+
       '<img src="https://image.ibb.co/i5Hp0a/LogoWeb.png" alt="">\n'+
     '</div>\n'+
-    '<div style="float:left;width:2px;height:100%;background-color:rgba(0,0,0,0.4);"></div>\n'+
+    '<div style="float:left;width:2px;height:100%;background-color:rgba(0,0,0,0.4)">'+
+      '<a id="linkToCurrentGitRepo">repo</a>'+
+    '</div>\n'+
     '<div style="float:right;width:2px;height:100%;background-color:rgba(0,0,0,0.4);"></div>\n'+
   '</nav>\n'+
   '<div style="clear:left"></div><br><br>');
