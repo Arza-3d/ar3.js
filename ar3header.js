8de72617d0c40332df20fe53a129675f40be1f3f
arza.3d <arza.3d@gmail.com>
2018-09-25 09:44:02 +0700

    go go auto header :D

--- ar3header.js
+++ ar3header.js
@@ -1,13 +1,21 @@
 (function() {
-  $('body').prepend('<nav id="nav-ar3" style="z-index:10;position:fixed;top:0px;left:0px;background-color:rgb(23,26,28);width:100%;height:45px;">\n'+
-    '<div style="width:158px;height:100%;background-color:rgba(0,0,0,0);float:left;">\n'+
-      '<img src="https://image.ibb.co/i5Hp0a/LogoWeb.png" alt="">\n'+
-    '</div>\n'+
-    '<div style="float:left;width:2px;height:100%;background-color:rgba(0,0,0,0.4)">'+
-      '<a id="linkToCurrentGitRepo">repo</a>'+
-    '</div>\n'+
-    '<div style="float:right;width:2px;height:100%;background-color:rgba(0,0,0,0.4);"></div>\n'+
-  '</nav>\n'+
-  '<div style="clear:left"></div><br><br>');
+    'use strict';
+    var linkToCurrentRepo = 'r3-linkToCurrentRepo';
+    $('body').prepend(
+        '<nav id="r3-nav" style="z-index:10;position:fixed;top:0px;left:0px;background-color:rgb(23,26,28);width:100%;height:45px;">\n'+
+            '<div style="width:158px;height:100%;background-color:rgba(0,0,0,0);float:left;">\n'+
+                '<img src="https://image.ibb.co/i5Hp0a/LogoWeb.png" alt="">\n'+
+            '</div>\n'+
+            '<div style="float:left;width:2px;height:100%;background-color:rgba(0,0,0,0.4)">'+
+            '<nav>'+
+                '<a id="'+ linkToCurrentRepo +'">repo</a>'+
+                '</nav>'+
+            '</div>\n'+
+            '<div style="float:right;width:2px;height:100%;background-color:rgba(0,0,0,0.4);"></div>\n'+
+        '</nav>\n'+
+        '<div style="clear:left"></div><br><br>'
+    );
+
+    $('#' + linkToCurrentRepo)
 })();
 // making header
