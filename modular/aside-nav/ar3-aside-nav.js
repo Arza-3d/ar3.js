1dc7807042c29b4d60626ae7479379eb976be022
arza.3d <arza.3d@gmail.com>
2018-09-27 05:34:38 +0700

    ...forgot this one

--- modular/aside-nav/ar3-aside-nav.js
+++ modular/aside-nav/ar3-aside-nav.js
@@ -1,5 +1,8 @@
 (function() {
     'use strict';
+    var constructNote = 'ar3-aside-nav_done';
+    var isConstructed = $('body').hasClass(constructNote);
+
     var $navTarget = $('.aside-nav-target-r3');
     var $headers = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'];
 
@@ -7,15 +10,17 @@
     $navTarget.after('<div style="clear:left">');
 
     for (var i = 0; i < $navTarget.length; i++) {
-
         var j = 0;
         while ($headers[j] !== undefined) {
 
-            var $h = $($headers[j]);
+            var $h = $($navTarget[i]).find($headers[j]);
                 for (var k = 0; k < $h.length; k++) {
+
+                    $($h[k]).addClass('ar3-h' + (j + 1));
+
                     if ($($h[k]).attr('id') === undefined) {
-                        $($h[k]).attr('id', 'h' + (j + 1) + '-' + (k + 1) + '_of_content_'+ (i + 1)).
-                            addClass('ar3-h' + (j + 1));
+                        $($h[k]).attr('id', 'h' + (j + 1) + '-' + (k + 1) + '_of_content_'+ (i + 1));
+                        console.log('h' + (j + 1) + '-' + (k + 1) + '_of_content_'+ (i + 1));
                     }
                 }
 
@@ -23,4 +28,5 @@
         }
     }
 
+    $('body').addClass(constructNote);
 })();
