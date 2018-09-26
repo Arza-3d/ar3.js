0a3fe638da9a9beee7a18a42f4c87a8e8b2ca7db
arza.3d <arza.3d@gmail.com>
2018-09-26 19:47:23 +0700

    navaside

--- modular/aside-nav/aside-nav-r3.js
+++ modular/aside-nav/aside-nav-r3.js
@@ -1,3 +1,10 @@
 (function() {
+    'use strict';
+    var $navTarget = $('.aside-nav-target-r3');
+    var $headers = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'];
+
+    for (var i = 0; i < $navTarget; i++) {
+        $navTarget.wrap('<div class="relative-container-r3">');
+    }
 
 })();
