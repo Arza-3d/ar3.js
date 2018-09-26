bd63b285394b6e172a904a8abd4d8f236cf4760a
arza.3d <arza.3d@gmail.com>
2018-09-27 06:32:50 +0700

    nav

--- modular/aside-nav/ar3-aside-nav.js
+++ modular/aside-nav/ar3-aside-nav.js
@@ -1,32 +1,43 @@
 (function() {
     'use strict';
-    var constructNote = 'ar3-aside-nav_done';
-    var isConstructed = $('body').hasClass(constructNote);
 
     var $navTarget = $('.aside-nav-target-r3');
-    var $headers = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'];
+    if ($navTarget.length !== 0) {
+        var constructNote = 'ar3-aside-nav_done';
+        var isConstructed = $('body').hasClass(constructNote);
 
-    $navTarget.wrap("<div class='relative-container-r3'></div>");
-    $navTarget.after('<div style="clear:left">');
+        var $headers = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'];
 
-    for (var i = 0; i < $navTarget.length; i++) {
-        var j = 0;
-        while ($headers[j] !== undefined) {
+        // create necessary wrap for dynamic aside
+        $navTarget.wrap("<div class='relative-container-r3'>");
+        $navTarget.after('<div style="clear:left">');
 
-            var $h = $($navTarget[i]).find($headers[j]);
-                for (var k = 0; k < $h.length; k++) {
+        // add class and id
+        for (var i = 0; i < $navTarget.length; i++) {
+            var j = 0;
 
-                    $($h[k]).addClass('ar3-h' + (j + 1));
+            // insert aside
+            $($navTarget[i]).before('<aside id="r3-aside-nav' + (i + 1) +'" class="r3-aside-nav">');
 
-                    if ($($h[k]).attr('id') === undefined) {
-                        $($h[k]).attr('id', 'h' + (j + 1) + '-' + (k + 1) + '_of_content_'+ (i + 1));
-                        console.log('h' + (j + 1) + '-' + (k + 1) + '_of_content_'+ (i + 1));
+            while ($headers[j] !== undefined) {
+                var $h = $($navTarget[i]).find($headers[j]);
+                    for (var k = 0; k < $h.length; k++) {
+
+                        $($h[k]).addClass('ar3-h' + (j + 1));
+
+                        if ($($h[k]).attr('id') === undefined) {
+                            $($h[k]).attr('id', 'h' + (j + 1) + '-' + (k + 1) + '_of_content_'+ (i + 1));
+                        }
+
+                        // create element
+                        $('').;
                     }
-                }
 
-            j++;
+                j++;
+            }
         }
+
+        $('body').addClass(constructNote);
     }
 
-    $('body').addClass(constructNote);
 })();
