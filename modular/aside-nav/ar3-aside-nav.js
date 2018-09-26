40f164a75032776fb62fdf8e8ca0db3f39ea8e2d
arza.3d <arza.3d@gmail.com>
2018-09-26 20:51:47 +0700

    nav

--- modular/aside-nav/ar3-aside-nav.js
+++ modular/aside-nav/ar3-aside-nav.js
@@ -3,9 +3,24 @@
     var $navTarget = $('.aside-nav-target-r3');
     var $headers = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'];
 
-    for (var i = 0; i < $navTarget; i++) {
-        $navTarget.wrap('<div class="relative-container-r3">');
-        $navTarget.after('<div style="clear:left">');
+    $navTarget.wrap("<div class='relative-container-r3'></div>");
+    $navTarget.after('<div style="clear:left">');
+
+    for (var i = 0; i < $navTarget.length; i++) {
+
+        var j = 0;
+        while ($headers[j] !== undefined) {
+
+            var $h = $($headers[j]);
+                for (var k = 0; k < $h.length; k++) {
+                    if ($($h[k]).attr('id') === undefined) {
+                        $($h[k]).attr('id', 'h' + (j + 1) + '-' + (k + 1) + '_of_content_'+ (i + 1)).
+                            addClass('ar3-h' + (j + 1));
+                    }
+                }
+
+            j++;
+        }
     }
 
 })();
