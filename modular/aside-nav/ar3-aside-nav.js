ea4ed2d3c04ac7269745be3dac8756039f6aa977
arza.3d <arza.3d@gmail.com>
2018-09-27 10:53:52 +0700

    ...

--- modular/aside-nav/ar3-aside-nav.js
+++ modular/aside-nav/ar3-aside-nav.js
@@ -6,7 +6,7 @@
         var constructNote = 'ar3-aside-nav_done';
         var isConstructed = $('body').hasClass(constructNote);
 
-        var $headers = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'];
+        var headers = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'];
 
         // create necessary wrap for dynamic aside
         $navTarget.wrap("<div class='relative-container-r3'>");
@@ -14,30 +14,38 @@
 
         // add class and id
         for (var i = 0; i < $navTarget.length; i++) {
-            var j = 0;
 
             // insert aside
             $($navTarget[i]).before('<aside id="r3-aside-nav-' + (i + 1) +'" class="r3-aside-nav">');
 
-            while ($headers[j] !== undefined) {
-                var $h = $($navTarget[i]).find($headers[j]);
-                //var comma = (linkTarget)
-                //var linkTarget += ',' +;
+            // create id and class for h1-h6
+            (function() {
+                var j = 0;
+                while (headers[j] !== undefined) {
+                    var $h = $($navTarget[i]).find(headers[j]);
 
-                    for (var k = 0; k < $h.length; k++) {
+                        for (var k = 0; k < $h.length; k++) {
 
-                        $($h[k]).addClass('ar3-h' + (j + 1));
+                            $($h[k]).addClass('ar3-h' + (j + 1));
 
-                        if ($($h[k]).attr('id') === undefined) {
-                            $($h[k]).attr('id', 'h' + (j + 1) + '-' + (k + 1) + '_of_content_'+ (i + 1));
+                            if ($($h[k]).attr('id') === undefined) {
+                                $($h[k]).attr('id', 'h' + (j + 1) + '-' + (k + 1) + '_of_content_'+ (i + 1));
+                            }
                         }
+                    j++;
+                }
+            })();
 
-                        // create element
-                        $('#r3-aside-nav-' + (i + 1)).append('<a href="#'+ $($h[k]).attr('id') +'">' + $($h[k]).html() + '</a>');
-                    }
+            // create element <a> in aside
+            (function() {
+                var $hInTarget = $($navTarget[i]).find(headers.toString());
+                for(var j = 0; j < $hInTarget.length; j++) {
 
-                j++;
-            }
+                    var $prevEl = (j !== 0) ? $($hInTarget[j - 1]) : undefined;
+
+                    $('#r3-aside-nav-' + (i + 1)).append('<a href="#'+ $($hInTarget[j]).attr('id') + '">' + $($hInTarget[j]).html() + '</a>');
+                }
+            })();
         }
 
         $('body').addClass(constructNote);
