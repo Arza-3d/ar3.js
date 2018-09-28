f42cc00725e716de6c432aaa80ae5fd0eb4e1e17
arza.3d <arza.3d@gmail.com>
2018-09-29 05:55:24 +0700

    ...

--- modular/trivial/ar3-trivial-tag.js
+++ modular/trivial/ar3-trivial-tag.js
@@ -16,6 +16,19 @@ https://arza-3d.github.io/ar3.js/
         // 1.
         $('table').wrap('<div style="overflow:auto">');
 
+        // 2.
+        {
+            const $img = $('img');
+            for (let i = 0; i < $img.length; i++) {
+                if ($img[i].parent().hasClass('relative-container-r3')) {
+                    $img[i].parent().css('overflow', 'auto');
+                } else {
+                    $img[i].wrap('<div style="overflow:auto">')
+                }
+            }
+        }
+
+
     }
 
     $('body').addClass(constructNote);
