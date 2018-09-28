934d1b57ff1e846d3e3eb108909a78ccc73e6cd2
arza.3d <arza.3d@gmail.com>
2018-09-28 20:16:48 +0700

    trivial-ar3 attr update

--- modular/trivial/ar3-trivial-attr.js
+++ modular/trivial/ar3-trivial-attr.js
@@ -16,12 +16,23 @@ https://arza-3d.github.io/ar3.js/
         $('header').append('<meta name="viewport" content="width=device-width, initial-scale=1">');
 
         // 2. if the page is not English, by default every <i> is english
-        {
-            const isEnglish = $('body').attr('lang') == 'en';
-            if (!isEnglish) {
-                $('i').attr('lang', 'en');
-            }
+        const isEnglish = $('body').attr('lang') == 'en';
+        if (!isEnglish) {
+            $('i').attr('lang', 'en');
         }
+
+        // 3. don't translate <code> and <pre>...
+        $('code, pre').attr('translate', 'no');
+
+        // 4. ...but translate .comment-r3
+        $('.comment-r3').attr('translate', 'yes');
+
+        // 5. make sure every video have control
+        $('video').attr('controls');
+
+        // 6. make sure every outside link open new table
+        $("a[href^='http']").attr('target', '_blank');
+
     }
 
     $('body').addClass(constructNote);
