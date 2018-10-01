f705c40db69bff6593128ba5f9a0cb9ab6dd6208
arza.3d <arza.3d@gmail.com>
2018-10-02 06:28:07 +0700

    it is challanging to untangling this aside code...

--- modular/aside-nav/ar3-aside-nav-3.js
+++ modular/aside-nav/ar3-aside-nav-3.js
@@ -8,90 +8,85 @@ https://arza-3d.github.io/ar3.js/
 --------------------------*/
 
 {
-    const $navTarget = $('.aside-nav-target-r3');
-    if ($navTarget.length !== 0) {
-        const constructNote = 'ar3-aside-nav_done',
-            isConstructed = $('body').hasClass(constructNote);
-
-        // update 2 start
-        let headers;
-        {
-            let newHeader = $('body').attr('data-headers-r3');
-            const isValid = newHeader !== undefined;
-            newHeader = (isValid) ? newHeader.split('|') : newHeader;
-            headers = (isValid) ? newHeader : ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'];
-        }
-        // update 2 end
-
-        // create necessary wrap for dynamic aside
-        if (!isConstructed) {
-            $navTarget.wrap("<div class='relative-container-r3'>");
-            $navTarget.after('<div style="clear:left">');
-        } else {
-            $navTarget.parent().children('.r3-aside-nav').remove();
-        }
-
-        // construct navigation
-        for (let i = 0; i < $navTarget.length; i++) {
-
-            // insert aside
-            $($navTarget[i]).before('<aside id="r3-aside-nav-' + (i + 1) + '" class="r3-aside-nav">');
-
-            // create id and class for h1-h6
-            {
-                let j = 0;
-                while (headers[j] !== undefined) {
-                    let $h = $($navTarget[i]).find(headers[j]);
-                        for (let k = 0; k < $h.length; k++) {
-                            $($h[k]).addClass('r3-h' + (j + 1));
-                            if ($($h[k]).attr('id') === undefined) {
-                                $($h[k]).attr('id', 'h' + (j + 1) + '-' + (k + 1) + '_of_content_'+ (i + 1));
-                            }
-                        }
-                    j++;
-                }
-            }
-
-            {
-                const $allH = $($navTarget[i]).find(headers.toString()),
-                      $h1st = $($allH[0]);
-                let $targetLinkParent = $('#r3-aside-nav-' + (i + 1));
-                const $linkParent0 = $targetLinkParent;
-                $targetLinkParent.append('<a href="#'+ $h1st.attr('id') + '">' + $h1st.html() + '</a>');
-
-                function createLink($header) {
-                    return $targetLinkParent.append('<a href="#'+ $header.attr('id') + '">' + $header.html() + '</a>');
-                }
-                let $hPrev = $h1st,
-                    hPrevClass = $hPrev.attr('class'),
-                    hPrevHierarchy = parseInt(hPrevClass.slice(4));
-                for (let j = 1; j < $allH.length; j++) {
-                    let $h = $($allH[j]),
-                        hClass = $h.attr('class'),
-                        hHierarchy = parseInt(hClass.slice(4));
-
-                    let hierarchyDiff = hPrevHierarchy - hHierarchy;
-                    if (hierarchyDiff  == 0) {
-                        createLink($h);
-                    } else if (hierarchyDiff  == -1) {
-                        $targetLinkParent.append('<nav class="r3-hierarchy-' + hPrevHierarchy + '"></nav>');
-                        $targetLinkParent = $targetLinkParent.find('nav:last-child');
-                        createLink($h);
-                    } else if (hierarchyDiff > 0) {
-                        if (hHierarchy == 1) {
-                            $targetLinkParent = $linkParent0;
-                            createLink($h);
-                        } else {
-                            $targetLinkParent = $linkParent0.find('.r3-hierarchy-' + (hHierarchy - 1) + ':last-child');
-                            createLink($h);
-                        }
-                    }
-                    $hPrev = $h;
-                    hPrevHierarchy = hHierarchy;
-                }
-            }
-        }
-
-        $('body').addClass(constructNote);
+    // get top coordinate of an element
+    function topCoord(elem) {
+        let y = elem.getBoundingClientRect().top + window.scrollY;
+        return y;
     }
+
+    function leftCoord(elem) {
+        let x = elem.getBoundingClientRect().left + window.scrollX;
+        return x;
+    }
+
+    // track current position
+    {
+
+        const $aside = $('.r3-aside-nav');
+        // create dummy aside
+        {
+            $aside.before('<div class="r3-dummy-aside">');
+        }
+        const $dummy = $('.r3-dummy-aside');
+
+        function scrollTopCoord() {
+            let top = 0;
+            if (typeof window.pageYOffset === "number") {
+                top = window.pageYOffset;
+            } else if (document.body && document.body.scrollTop) {
+                top = document.body.scrollTop;
+            } else if (document.documentElement && document.documentElement.scrollTop) {
+                top = document.documentElement.scrollTop;
+            }
+            return top;
+        }
+
+        const $main = $('.aside-nav-target-r3');
+
+        function dynamicAside() {
+            let topLimit = topCoord($main[0]);
+            let bottomLimit = topLimit + $main[0].scrollHeight - window.innerHeight;
+            console.log(leftCoord($main[0]));
+            if (scrollTopCoord() < topLimit) {
+                $aside.removeClass('r3-dynamic-on')
+                      .removeClass('r3-dynamic-after');
+                $dummy.removeClass('r3-active');
+            } else if (scrollTopCoord() < bottomLimit) {
+                $aside.removeClass('r3-dynamic-after')
+                      .addClass('r3-dynamic-on');
+                $dummy.addClass('r3-active');
+            } else {
+                $aside.removeClass('r3-dynamic-on')
+                      .addClass('r3-dynamic-after');
+                $dummy.addClass('r3-active');
+            }
+        }
+        dynamicAside();
+
+        document.addEventListener('click', dynamicAside);
+        document.addEventListener('resize', dynamicAside);
+        document.addEventListener('scroll', dynamicAside);
+
+        /*
+        // set dynamic header for ar3header
+        {
+            function dynamicHeader() {
+                let $nav = $('#r3-nav'),
+                    topLimit = topCoord($main[0]) - $nav.innerHeight() - 15;
+                if (scrollTopCoord() < topLimit) {
+                    $nav.css({'position': 'fixed', 'top' : 0});
+                } else if (scrollTopCoord() > topLimit){
+                    $nav.css({'position': 'absolute', 'top': topLimit + 'px'});
+                }
+            }
+          dynamicHeader();
+
+          document.addEventListener('click', dynamicHeader);
+          document.addEventListener('resize', dynamicHeader);
+          document.addEventListener('scroll', dynamicHeader);
+        }
+        */
+    }
+
+
 }
