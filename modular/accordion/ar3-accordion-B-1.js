439f9d2a996dbfb60de22a23bb1d76e0f33a4e84
arza.3d <arza.3d@gmail.com>
2018-10-01 12:11:19 +0700

    1st accordion type B is done

---
+++ modular/accordion/ar3-accordion-B-1.js
@@ -0,0 +1,15 @@
+/*######################
+// ar3-accordion-B.js  #
+######################*/
+/*--------------------------
+https://arza-3d.github.io/ar3.js/
+
+<script src="https://rawgit.com/Arza-3d/ar3.js/master/modular/accordion/ar3-accordion-B.js"></script>
+--------------------------*/
+
+{
+    $('.r3-accordion-B').click(function() {
+        $(this).toggleClass('r3-active')
+            .next().next().slideToggle('slow');
+    });
+}
