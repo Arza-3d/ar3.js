619752390308066a37302b655ca246243ada389c
arza.3d <arza.3d@gmail.com>
2018-10-05 19:50:15 +0700

    almost done nav-aside 4 60% 🔥

--- modular/aside-nav/ar3-aside-nav-4.js
+++ modular/aside-nav/ar3-aside-nav-4.js
@@ -8,9 +8,79 @@ https://arza-3d.github.io/ar3.js/
 --------------------------*/
 
 {
-    $('.r3-aside-nav').after('<button class="r3-nav-button" style="position:fixed">nav</button>');
-    $('.r3-nav-button').click(function() {
-        $(this).prev('.r3-aside-nav').toggleClass('r3-active');
+    // get top coordinate of an element
+    function topCoord(elem) {
+        let y = elem.getBoundingClientRect().top + window.scrollY;
+        return y;
+    }
 
-    });
+    function leftCoord(elem) {
+        let x = elem.getBoundingClientRect().left + window.scrollX;
+        return x;
+    }
+
+    // track current position
+    {
+
+        const $aside = $('.r3-aside-nav');
+        // create dummy aside
+        {
+            $aside.before('<div class="r3-aside-replacement">');
+        }
+        const $dummy = $('.r3-aside-replacement');
+
+        function scrollTopCoord() {
+            let top = 0;
+            if (typeof window.pageYOffset === "number") {
+                top = window.pageYOffset;
+            } else if (document.body && document.body.scrollTop) {
+                top = document.body.scrollTop;
+            } else if (document.documentElement && document.documentElement.scrollTop) {
+                top = document.documentElement.scrollTop;
+            }
+            return top;
+        }
+
+        const $main = $('.aside-nav-target-r3');
+
+        function setLocation(elem, $anchor) {
+            $(elem).css({
+                'top' : topCoord($anchor[0]),
+                'left' : leftCoord($anchor[0]) + $anchor[0].getBoundingClientRect().width
+            });
+        }
+
+        $('.r3-aside-nav-button').remove();
+        {
+            $('body').prepend('<a class="r3-aside-nav-button r3-active">...</a>');
+            setLocation('.r3-aside-nav-button', $aside);
+        }
+
+        function dynamicAside() {
+            let topLimit = topCoord($main[0]);
+            let bottomLimit = topLimit + $main[0].scrollHeight - window.innerHeight;
+            setLocation('.r3-aside-nav-button', $aside);
+            if (scrollTopCoord() < topLimit) {
+                $aside.removeClass('r3-dynamic-on')
+                      .removeClass('r3-dynamic-after');
+                $dummy.removeClass('r3-active');
+            } else if (scrollTopCoord() < bottomLimit) {
+                $aside.removeClass('r3-dynamic-after')
+                      .addClass('r3-dynamic-on');
+                $dummy.addClass('r3-active');
+            } else {
+                $aside.removeClass('r3-dynamic-on')
+                      .addClass('r3-dynamic-after');
+                $dummy.addClass('r3-active');
+            }
+        }
+        dynamicAside();
+
+        document.addEventListener('click', dynamicAside);
+        window.addEventListener('resize', dynamicAside);
+        document.addEventListener('scroll', dynamicAside);
+
+    }
+
+
 }
