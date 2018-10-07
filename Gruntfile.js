241bcde8188f3ac9d1921e9cd01693f283b9d758
arza.3d <arza.3d@gmail.com>
2018-10-07 20:17:50 +0700

    it worked

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,18 +14,32 @@ module.exports = function (grunt) {
           }
         }
       }
-  });*/
+    });*/
 
     grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.initConfig({
-      concat: {
-        options: {
-          separator: '\n\n// r3-separator\n\n',
+        concat: {
+            options: {
+                separator: '\n\n// r3-separator\n\n',
+            },
+            dist: {
+                src: [
+                    'modular/aside-nav/ar3-aside-nav-2.js',
+                    'modular/aside-nav/ar3-aside-nav-4.js',
+                    'modular/aside-nav/ar3-aside-nav-5.js',
+                    'accordion/ar3-accordion-B-1.js'
+                ],
+                dest: 'ar3-aside-concat.js',
+            },
         },
-        dist: {
-          src: ['modular/aside-nav/ar3-aside-nav-2.js', 'modular/aside-nav/ar3-aside-nav-4.js', 'modular/aside-nav/ar3-aside-nav-5.js'],
-          dest: 'ar3-aside-concat.js',
-        },
-      },
+
+        uglify: {
+            my_target: {
+                files: {
+                    'ar3.min.js': ['ar3-aside-concat.js']
+                }
+            }
+        }
     });
 };
