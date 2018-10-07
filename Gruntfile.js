7ba8f2fd2edfe52375b85e55bcfe3774171b276a
arza.3d <arza.3d@gmail.com>
2018-10-07 19:02:06 +0700

    🌺 at least the grunt concat work

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,4 +2,30 @@ module.exports = function (grunt) {
     grunt.registerTask('default', '', function(){
         grunt.log.write('test calling grunt task');
     });
+
+    // Project configuration.
+    /*
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.initConfig({
+      uglify: {
+        my_target: {
+          files: {
+            'ar3.min.js': ['modular/aside-nav/ar3-aside-nav-2.js']
+          }
+        }
+      }
+  });*/
+
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.initConfig({
+      concat: {
+        options: {
+          separator: '\n\n// r3-separator\n\n',
+        },
+        dist: {
+          src: ['modular/aside-nav/ar3-aside-nav-2.js', 'modular/aside-nav/ar3-aside-nav-4.js', 'modular/aside-nav/ar3-aside-nav-5.js'],
+          dest: 'ar3-aside-concat.js',
+        },
+      },
+    });
 };
