d4ace15ccdedbd14cc6416c4e24e6733168b4872
arza.3d <arza.3d@gmail.com>
2018-10-08 16:57:26 +0700

    🔥 side-nav

--- ar3-concat.js
+++ ar3-concat.js
@@ -1,3 +1,5 @@
+'use strict';
+
 /*######################
 // ar3-aside-nav-2.js  #
 ######################*/
@@ -8,18 +10,18 @@ https://arza-3d.github.io/ar3.js/
 --------------------------*/
 
 {
-    const $navTarget = $('.aside-nav-target-r3');
+    var $navTarget = $('.aside-nav-target-r3');
     if ($navTarget.length !== 0) {
-        const constructNote = 'ar3-aside-nav_done',
+        var constructNote = 'ar3-aside-nav_done',
             isConstructed = $('body').hasClass(constructNote);
 
         // update 2 start
-        let headers;
+        var headers = void 0;
         {
-            let newHeader = $('body').attr('data-headers-r3');
-            const isValid = newHeader !== undefined;
-            newHeader = (isValid) ? newHeader.split('|') : newHeader;
-            headers = (isValid) ? newHeader : ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'];
+            var newHeader = $('body').attr('data-headers-r3');
+            var isValid = newHeader !== undefined;
+            newHeader = isValid ? newHeader.split('|') : newHeader;
+            headers = isValid ? newHeader : ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'];
         }
         // update 2 end
 
@@ -32,70 +34,72 @@ https://arza-3d.github.io/ar3.js/
         }
 
         // construct navigation
-        for (let i = 0; i < $navTarget.length; i++) {
+        for (var i = 0; i < $navTarget.length; i++) {
 
             // insert aside
             $($navTarget[i]).before('<aside id="r3-aside-nav-' + (i + 1) + '" class="r3-aside-nav">');
 
             // create id and class for h1-h6
             {
-                let j = 0;
+                var j = 0;
                 while (headers[j] !== undefined) {
-                    let $h = $($navTarget[i]).find(headers[j]);
-                        for (let k = 0; k < $h.length; k++) {
-                            $($h[k]).addClass('r3-h' + (j + 1));
-                            if ($($h[k]).attr('id') === undefined) {
-                                $($h[k]).attr('id', 'h' + (j + 1) + '-' + (k + 1) + '_of_content_'+ (i + 1));
-                            }
+                    var $h = $($navTarget[i]).find(headers[j]);
+                    for (var k = 0; k < $h.length; k++) {
+                        $($h[k]).addClass('r3-h' + (j + 1));
+                        if ($($h[k]).attr('id') === undefined) {
+                            $($h[k]).attr('id', 'h' + (j + 1) + '-' + (k + 1) + '_of_content_' + (i + 1));
                         }
+                    }
                     j++;
                 }
             }
 
             {
-                const $allH = $($navTarget[i]).find(headers.toString()),
-                      $h1st = $($allH[0]);
-                let $targetLinkParent = $('#r3-aside-nav-' + (i + 1));
-                const $linkParent0 = $targetLinkParent;
-                $targetLinkParent.append('<a href="#'+ $h1st.attr('id') + '">' + $h1st.html() + '</a>');
+                (function () {
+                    var createLink = function createLink($header) {
+                        return $targetLinkParent.append('<a href="#' + $header.attr('id') + '">' + $header.html() + '</a>');
+                    };
 
-                function createLink($header) {
-                    return $targetLinkParent.append('<a href="#'+ $header.attr('id') + '">' + $header.html() + '</a>');
-                }
-                let $hPrev = $h1st,
-                    hPrevClass = $hPrev.attr('class'),
-                    hPrevHierarchy = parseInt(hPrevClass.slice(4));
-                for (let j = 1; j < $allH.length; j++) {
-                    let $h = $($allH[j]),
-                        hClass = $h.attr('class'),
-                        hHierarchy = parseInt(hClass.slice(4));
+                    var $allH = $($navTarget[i]).find(headers.toString()),
+                        $h1st = $($allH[0]);
+                    var $targetLinkParent = $('#r3-aside-nav-' + (i + 1));
+                    var $linkParent0 = $targetLinkParent;
+                    $targetLinkParent.append('<a href="#' + $h1st.attr('id') + '">' + $h1st.html() + '</a>');
 
-                    let hierarchyDiff = hPrevHierarchy - hHierarchy;
-                    if (hierarchyDiff  == 0) {
-                        createLink($h);
-                    } else if (hierarchyDiff  == -1) {
-                        $targetLinkParent.append('<nav class="r3-hierarchy-' + hPrevHierarchy + '"></nav>');
-                        $targetLinkParent = $targetLinkParent.find('nav:last-child');
-                        createLink($h);
-                    } else if (hierarchyDiff > 0) {
-                        if (hHierarchy == 1) {
-                            $targetLinkParent = $linkParent0;
-                            createLink($h);
-                        } else {
-                            $targetLinkParent = $linkParent0.find('.r3-hierarchy-' + (hHierarchy - 1) + ':last-child');
-                            createLink($h);
+                    var $hPrev = $h1st,
+                        hPrevClass = $hPrev.attr('class'),
+                        hPrevHierarchy = parseInt(hPrevClass.slice(4));
+                    for (var _j = 1; _j < $allH.length; _j++) {
+                        var _$h = $($allH[_j]),
+                            hClass = _$h.attr('class'),
+                            hHierarchy = parseInt(hClass.slice(4));
+
+                        var hierarchyDiff = hPrevHierarchy - hHierarchy;
+                        if (hierarchyDiff == 0) {
+                            createLink(_$h);
+                        } else if (hierarchyDiff == -1) {
+                            $targetLinkParent.append('<nav class="r3-hierarchy-' + hPrevHierarchy + '"></nav>');
+                            $targetLinkParent = $targetLinkParent.find('nav:last-child');
+                            createLink(_$h);
+                        } else if (hierarchyDiff > 0) {
+                            if (hHierarchy == 1) {
+                                $targetLinkParent = $linkParent0;
+                                createLink(_$h);
+                            } else {
+                                $targetLinkParent = $linkParent0.find('.r3-hierarchy-' + (hHierarchy - 1) + ':last-child');
+                                createLink(_$h);
+                            }
                         }
+                        $hPrev = _$h;
+                        hPrevHierarchy = hHierarchy;
                     }
-                    $hPrev = $h;
-                    hPrevHierarchy = hHierarchy;
-                }
+                })();
             }
         }
 
         $('body').addClass(constructNote);
     }
 }
-
 
 // r3-separator
 
@@ -110,25 +114,24 @@ https://arza-3d.github.io/ar3.js/
 
 {
     // get top coordinate of an element
-    function topCoord(elem) {
-        let y = elem.getBoundingClientRect().top + window.scrollY;
+    var topCoord = function topCoord(elem) {
+        var y = elem.getBoundingClientRect().top + window.scrollY;
         return y;
-    }
+    };
 
-    function leftCoord(elem) {
-        let x = elem.getBoundingClientRect().left + window.scrollX;
+    var leftCoord = function leftCoord(elem) {
+        var x = elem.getBoundingClientRect().left + window.scrollX;
         return x;
-    }
+    };
 
     // track current position
+
+
     {
-        const $aside = $('.r3-aside-nav');
-        $aside.before('<div class="r3-aside-replacement">');
-        const $dummy = $('.r3-aside-replacement');
 
         // get current scroll top = 0 coordinate
-        function scrollTopCoord() {
-            let top = 0;
+        var scrollTopCoord = function scrollTopCoord() {
+            var top = 0;
             if (typeof window.pageYOffset === "number") {
                 top = window.pageYOffset;
             } else if (document.body && document.body.scrollTop) {
@@ -137,22 +140,47 @@ https://arza-3d.github.io/ar3.js/
                 top = document.documentElement.scrollTop;
             }
             return top;
-        }
+        };
 
-        const $main = $('.aside-nav-target-r3');
-
-        function setLocation(elem, $anchor) {
+        var setLocation = function setLocation(elem, $anchor) {
             $(elem).css({
-                'top' : topCoord($anchor[0]) + $anchor[0].getBoundingClientRect().height * 0.25,
-                'left' : leftCoord($anchor[0]) + $anchor[0].getBoundingClientRect().width
+                'top': topCoord($anchor[0]) + $anchor[0].getBoundingClientRect().height * 0.25,
+                'left': leftCoord($anchor[0]) + $anchor[0].getBoundingClientRect().width
             });
-        }
+        };
 
-        function setButtonLocation() {
+        var setButtonLocation = function setButtonLocation() {
             setLocation('.r3-aside-nav-button', $aside);
-        }
+        };
 
-        let $button = $('.r3-aside-nav-button');
+        var dynamicAside = function dynamicAside() {
+            var topLimit = topCoord($main[0]);
+            var bottomLimit = topLimit + $main[0].scrollHeight - window.innerHeight;
+            setLocation('.r3-aside-nav-button', $aside);
+            if (scrollTopCoord() < topLimit) {
+                $aside.removeClass('r3-dynamic-on').removeClass('r3-dynamic-after');
+                $dummy.removeClass('r3-active');
+            } else if (scrollTopCoord() < bottomLimit) {
+                $aside.removeClass('r3-dynamic-after').addClass('r3-dynamic-on');
+                $dummy.addClass('r3-active');
+            } else {
+                $aside.removeClass('r3-dynamic-on').addClass('r3-dynamic-after');
+                $dummy.addClass('r3-active');
+            }
+        };
+
+        // make sure it is the same with the _r3-aside-nav-1.scss
+        var stopUpdateButton = function stopUpdateButton() {
+            clearInterval(buttonMove);
+        };
+
+        var $aside = $('.r3-aside-nav');
+        $aside.before('<div class="r3-aside-replacement">');
+        var $dummy = $('.r3-aside-replacement');
+
+        var $main = $('.aside-nav-target-r3');
+
+        var $button = $('.r3-aside-nav-button');
         $button.remove();
         {
             $('body').prepend('<a class="r3-aside-nav-button r3-active">&#9664;</a>');
@@ -160,24 +188,6 @@ https://arza-3d.github.io/ar3.js/
         }
         $button = $('.r3-aside-nav-button');
 
-        function dynamicAside() {
-            let topLimit = topCoord($main[0]);
-            let bottomLimit = topLimit + $main[0].scrollHeight - window.innerHeight;
-            setLocation('.r3-aside-nav-button', $aside);
-            if (scrollTopCoord() < topLimit) {
-                $aside.removeClass('r3-dynamic-on')
-                      .removeClass('r3-dynamic-after');
-                $dummy.removeClass('r3-active');
-            } else if (scrollTopCoord() < bottomLimit) {
-                $aside.removeClass('r3-dynamic-after')
-                      .addClass('r3-dynamic-on');
-                $dummy.addClass('r3-active');
-            } else {
-                $aside.removeClass('r3-dynamic-on')
-                      .addClass('r3-dynamic-after');
-                $dummy.addClass('r3-active');
-            }
-        }
         dynamicAside();
 
         document.addEventListener('click', dynamicAside);
@@ -185,15 +195,14 @@ https://arza-3d.github.io/ar3.js/
         $('.aside-nav-target-r3')[0].addEventListener('resize', dynamicAside);
         document.addEventListener('scroll', dynamicAside);
 
-        let buttonMove,
-            buttonStop;
-        const marginLeftTransition = 500,
-              mainDefaultWidth = '79%'; // make sure it is the same with the _r3-aside-nav-1.scss
-        function stopUpdateButton() {
-            clearInterval(buttonMove);
-        }
-        $button.click(function() {
-            if (buttonStop !== undefined) {clearTimeout(buttonStop)}
+        var buttonMove = void 0,
+            buttonStop = void 0;
+        var marginLeftTransition = 500,
+            mainDefaultWidth = '79%';
+        $button.click(function () {
+            if (buttonStop !== undefined) {
+                clearTimeout(buttonStop);
+            }
             $button.toggleClass('r3-active');
             if ($('.r3-aside-nav-button').hasClass('r3-active')) {
                 $button.html('&#9664;');
@@ -201,17 +210,14 @@ https://arza-3d.github.io/ar3.js/
                 $main.css('width', mainDefaultWidth);
             } else {
                 $button.html('&#9658;');
-                $aside.add($dummy).css('margin-left', (-1 * $aside[0].getBoundingClientRect().width) + 'px');
+                $aside.add($dummy).css('margin-left', -1 * $aside[0].getBoundingClientRect().width + 'px');
                 $main.css('width', '95%');
             }
             buttonMove = setInterval(setButtonLocation, 1);
             buttonStop = setTimeout(stopUpdateButton, marginLeftTransition);
         });
-
     }
-
 }
-
 
 // r3-separator
 
@@ -225,14 +231,9 @@ https://arza-3d.github.io/ar3.js/
 --------------------------*/
 
 {
-    $('.r3-aside-nav').find('a + nav').prev()
-        .before('<div class="r3-accordion-B"></div>');
+    $('.r3-aside-nav').find('a + nav').prev().before('<div class="r3-accordion-B"></div>');
 }
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 5459a7ca922eb2a9f436554b30d0e7d3d580c0f3
 // r3-separator
 
 /*######################
@@ -245,7 +246,6 @@ https://arza-3d.github.io/ar3.js/
 --------------------------*/
 
 {
-<<<<<<< HEAD
     var accordTarget = $('body').attr('data-accordion-B-r3'),
         _isValid = accordTarget !== undefined;
     if (_isValid) {
@@ -254,20 +254,8 @@ https://arza-3d.github.io/ar3.js/
 
     $('.r3-accordion-B').click(function () {
         $(this).toggleClass('r3-active').next().next().slideToggle('slow');
-=======
-    let accordTarget = $('body').attr('data-accordion-B-r3'),
-        isValid = accordTarget !== undefined;
-    if (isValid) {
-        $(accordTarget).addClass('r3-accordion-B');
-    }
-
-    $('.r3-accordion-B').click(function() {
-        $(this).toggleClass('r3-active')
-            .next().next().slideToggle('slow');
->>>>>>> 5459a7ca922eb2a9f436554b30d0e7d3d580c0f3
     });
 }
-
 
 // r3-separator
 
@@ -281,7 +269,6 @@ https://arza-3d.github.io/ar3.js/
 --------------------------*/
 
 {
-<<<<<<< HEAD
     var _accordTarget = $('body').attr('data-accordion-r3'),
         _isValid2 = _accordTarget !== undefined;
     if (_isValid2) {
@@ -289,19 +276,9 @@ https://arza-3d.github.io/ar3.js/
     }
 
     $('.r3-accordion').click(function () {
-=======
-    let accordTarget = $('body').attr('data-accordion-r3'),
-        isValid = accordTarget !== undefined;
-    if (isValid) {
-        $(accordTarget).addClass('r3-accordion');
-    }
-
-    $('.r3-accordion').click(function() {
->>>>>>> 5459a7ca922eb2a9f436554b30d0e7d3d580c0f3
         $(this).toggleClass('r3-active').next().slideToggle('slow');
     });
 }
-
 
 // r3-separator
 
@@ -315,16 +292,16 @@ https://arza-3d.github.io/ar3.js/
 --------------------------*/
 
 {
-    const constructNote = 'ar3-trivial-attr_is_constructed',
-        isConstructed = $('body').hasClass(constructNote);
+    var _constructNote = 'ar3-trivial-attr_is_constructed',
+        _isConstructed = $('body').hasClass(_constructNote);
 
-    if (!isConstructed) {
+    if (!_isConstructed) {
 
         // 1.
         $('header').append('<meta name="viewport" content="width=device-width, initial-scale=1">');
 
         // 2.
-        const isEnglish = $('body').attr('lang') == 'en';
+        var isEnglish = $('body').attr('lang') == 'en';
         if (!isEnglish) {
             $('i').attr('lang', 'en');
         }
@@ -340,64 +317,9 @@ https://arza-3d.github.io/ar3.js/
 
         // 6.
         $("a[href^='http']").attr('target', '_blank');
-
     }
 
-    $('body').addClass(constructNote);
-}
-
-
-// r3-separator
-
-/*######################
-// ar3-trivial-tag.js  #
-######################*/
-/*--------------------------
-https://arza-3d.github.io/ar3.js/
-
-<script src="https://rawgit.com/Arza-3d/ar3.js/master/modular/trivial/ar3-trivial-tag.js"></script>
---------------------------*/
-
-{
-    const constructNote = 'ar3-trivial-tag_is_constructed',
-        isConstructed = $('body').hasClass(constructNote);
-
-    if (!isConstructed) {
-
-        // 1.
-        $('table').wrap('<div style="overflow:auto">');
-
-        // 2.
-        {
-            const $img = $('img');
-            for (let i = 0; i < $img.length; i++) {
-                if ($($img[i]).parent().hasClass('relative-container-r3')) {
-                    $($img[i]).parent().css('overflow', 'auto');
-                } else {
-                    $($img[i]).wrap('<div style="overflow:auto">')
-                }
-            }
-        }
-
-        // 3. add hr between section
-        $('main section + h3, aside > nav + .r3-accordion-B').before('<hr>');
-
-        // 4. add contextual title based on the main > div[data-title-r3]
-        {
-            const $titles = $('main > div');
-            let title, id;
-
-            for (let i = 0; i < $titles.length; i++) {
-                title = $($titles[i]).attr('data-title-r3');
-
-                id = $($titles[i]).find('> h2:first-child').attr('id');
-                $('aside > a[href="#'+ id +'"]').prev('.r3-accordion-B')
-                    .before('<h3>'+ title +'</h3>');
-            }
-        }
-    }
-
-    $('body').addClass(constructNote);
+    $('body').addClass(_constructNote);
 }
 
 // r3-separator
