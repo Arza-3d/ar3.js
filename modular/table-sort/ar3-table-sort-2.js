9d688bb07f9b31c73f0364a0de63ee18888ad06e
Forest Guhareza <arza.3d@gmail.com>
2018-12-23 20:04:39 +0700

    fix the first row problem

--- modular/table-sort/ar3-table-sort-2.js
+++ modular/table-sort/ar3-table-sort-2.js
@@ -11,44 +11,64 @@ https://arza-3d.github.io/ar3.js/
     let tables, rows, switching, h, i1, x0, y0, x, y, shouldSwitch;
     tables = document.getElementsByClassName("sort-r3");
     switching = true;
-    function switchTableRow() {
-      for (h = 0; h < tables.length; h++) {
 
-          while (switching) {
-              switching = false;
-              rows = tables[h].querySelectorAll("tbody tr");
-              for (i = 1; i < (rows.length - 1); i++) {
-                  shouldSwitch = false;
-                  x0 = rows[i].getElementsByTagName("TD")[0].innerHTML.toLowerCase();
-                  if (/<!--/.test(x0)) {
-                      i1 = x0.indexOf(' -->');
-                      x = x0.slice(5, i1);
-                  } else {
-                      x = x0;
-                  }
-
-                  y0 = rows[i + 1].getElementsByTagName("TD")[0].innerHTML.toLowerCase();
-                  if (/<!--/.test(y0)) {
-                      i1 = y0.indexOf(' -->');
-                      y = y0.slice(5, i1);
-                  } else {
-                      y = y0;
-                  }
-
-                  if (x > y) {
-                      shouldSwitch= true;
-                      break;
-                  }
-              }
-              if (shouldSwitch) {
-                  rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-                  switching = true;
-              }
-          }
-          switching = true;
+    function getComment(text) {
+      let i_end;
+      if (/<!--/.test(text)) {
+          i_end = text.indexOf(' -->');
+          return text.slice(5, i_end);
+      } else {
+          return text;
       }
     }
 
-    switchTableRow();
+    function switchTablesRow() {
+
+      for (h = 0; h < tables.length; h++) {
+
+
+          function switchTableRow() {
+            while (switching) {
+                switching = false;
+                rows = tables[h].querySelectorAll("tbody > tr");
+                for (i = 1; i < (rows.length - 1); i++) {
+                    shouldSwitch = false;
+                    x0 = rows[i].getElementsByTagName("TD")[0].innerHTML.toLowerCase();
+                    x = getComment(x0);
+
+                    y0 = rows[i + 1].getElementsByTagName("TD")[0].innerHTML.toLowerCase();
+                    y = getComment(y0);
+
+                    if (x > y) {
+                        shouldSwitch= true;
+                        break;
+                    }
+                }
+                if (shouldSwitch) {
+                    rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+                    switching = true;
+                }
+            }
+            switching = true;
+          }
+          switchTableRow();
+
+          // fix the first row
+          let row1test, row2test;
+          row1test = tables[h].querySelector("tbody > tr:nth-child(1) > td:nth-child(1)").innerHTML.toLowerCase();
+          row2test = tables[h].querySelector("tbody > tr:nth-child(2) > td:nth-child(1)").innerHTML.toLowerCase();
+          if (getComment(row1test) > getComment(row2test)) {
+            x = tables[h].querySelector("tbody > tr:nth-child(1)").innerHTML;
+            y = tables[h].querySelector("tbody > tr:nth-child(2)").innerHTML;
+            tables[h].querySelector("tbody > tr:nth-child(1)").innerHTML = y;
+            tables[h].querySelector("tbody > tr:nth-child(2)").innerHTML = x;
+            switchTableRow();
+          }
+
+
+      }
+    }
+
+    switchTablesRow();
 
 })();
