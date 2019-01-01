44cbad413ed8e86b5fe5843d0ca6f228d50f6f1d
Forest Guhareza <arza.3d@gmail.com>
2019-01-01 12:20:29 +0700

    why I don't use module yet

--- modular/module/test-ar3-module-1.js
+++ modular/module/test-ar3-module-1.js
@@ -6,10 +6,28 @@
 
     https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/import
   */
-  
+  let module_1 = (window.location.href.indexOf('localhost') > -1) ? '/ar3js/modular/module/ar3-module-1.js' : 'ar3-module-1.js';
+  console.log(typeof module_1);
+
+  /*
+  import {sum2Arrays} from module_1;
+  import * as myModule from module_1;
+  */
+  /*################
+  NOTE: I am not using module,
+  because:
+    1. can't be tested offline, should use local server
+    2. link of localhost and the online one of the module should be different
+    3. can't change the 'module_1' / module address into variable that contains string....
+    4. I hope they fix this in the future
+  *///################
+
   import {sum2Arrays} from '/ar3js/modular/module/ar3-module-1.js';
   import * as myModule from '/ar3js/modular/module/ar3-module-1.js';
-
+  //alert(0);
   console.log(myModule.ArrX);
   console.log(myModule.cepot);
   console.log(sum2Arrays);
+
+  alert(window.location.href);
+  alert(1);
