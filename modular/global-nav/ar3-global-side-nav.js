ec2955b5469c4e95e3f3de3b6644105fe60882f9
Forest Guhareza <arza.3d@gmail.com>
2020-09-13 22:46:36 +0700

    test

--- modular/global-nav/ar3-global-side-nav.js
+++ modular/global-nav/ar3-global-side-nav.js
@@ -1,10 +1,23 @@
 {
   let globalNav = `
-    <nav class = "r3-global-side-nav" id="ar3SideNav">
-      <a href="http://google.com" target="_blank">google</a>
-      <a href="http:yahoo.com" target="_blank">test</a>
+    <nav class = "r3-global-side-nav" id="ar3-global-side-nav">
+      <button href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</button>
+      <a href="https://google.com" target="_blank">google</a>
+      <a href="https://yahoo.com" target="_blank">test</a>
     </nav>
+
+    <button onclick="openNav()">open</button>
   `;
   $(".r3-global-nav").html("This is global nav" + globalNav);
+
+  /* Set the width of the side navigation to 250px */
+  function openNav() {
+    document.getElementById("ar3-global-side-nav").style.width = "250px";
+  }
+
+  /* Set the width of the side navigation to 0 */
+  function closeNav() {
+    document.getElementById("ar3-global-side-nav").style.width = "0";
+  }
 
 }
