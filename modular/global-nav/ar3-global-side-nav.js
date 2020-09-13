9547e1036dd698ab37e395c155e2bb39ee55ed4d
Forest Guhareza <arza.3d@gmail.com>
2020-09-13 15:40:55 +0700

    ....

---
+++ modular/global-nav/ar3-global-side-nav.js
@@ -0,0 +1,10 @@
+{
+  let globalNav = `
+    <nav>
+      <a href="http://google.com">google</a>
+      <a href="yahoo.com">test</a>
+    </nav>
+  `;
+  $(".r3-global-nav").html("This is global nav");
+
+}
