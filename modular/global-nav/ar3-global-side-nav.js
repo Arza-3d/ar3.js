e8b2e97e9221191b8d0e923a6fef5f16cc513788
Forest Guhareza <arza.3d@gmail.com>
2020-09-13 15:46:04 +0700

    test_

--- modular/global-nav/ar3-global-side-nav.js
+++ modular/global-nav/ar3-global-side-nav.js
@@ -1,8 +1,8 @@
 {
   let globalNav = `
     <nav>
-      <a href="http://google.com">google</a>
-      <a href="yahoo.com">test</a>
+      <a href="http://google.com" target="_blank">google</a>
+      <a href="http:yahoo.com" target="_blank">test</a>
     </nav>
   `;
   $(".r3-global-nav").html("This is global nav" + globalNav);
